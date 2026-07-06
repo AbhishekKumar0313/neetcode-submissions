b5c65499fd900b1a488405a6f2e73e10217a1fca
Abhishek Kumar <99200063+AbhishekKumar0313@users.noreply.github.com>
2026-07-06 22:24:11 +0530

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.py
@@ -0,0 +1,31 @@
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.isend=False
+class Trie:
+    def __init__(self):
+        self.root=TrieNode()
+
+    def insert(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char]=TrieNode()
+            node=node.children[char]
+        node.isend=True
+    
+    def lcp(self,node,res):
+        if node.isend==True or len(node.children)!=1:
+            return res
+        char = next(iter(node.children))
+        res.append(char)
+        self.lcp(node.children[char],res)
+        return res
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        trie=Trie()
+        for string in strs:
+            trie.insert(string)
+        res=trie.lcp(trie.root,[])
+        return ''.join(res)
