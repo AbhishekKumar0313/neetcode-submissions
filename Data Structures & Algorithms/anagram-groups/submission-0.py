26c76d422e1f1af237487bd841fa2aaa74477c21
Abhishek Kumar <99200063+AbhishekKumar0313@users.noreply.github.com>
2026-07-07 07:47:57 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def checkAnagram(self,curr,check):
+        if len(curr)!=len(check):
+            return False
+        array=[0]*26
+        for i in range(len(curr)):
+            array[ord(curr[i])-97]+=1
+            array[ord(check[i])-97]-=1
+        for val in array:
+            if val !=0:
+                return False
+        return True
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=[]
+        while True:
+            if len(strs)>0:
+                curr=strs[0]
+                temp=[]
+                i=0
+                while i<len(strs):
+                    if self.checkAnagram(curr,strs[i]):
+                        temp.append(strs[i])
+                        strs.pop(i)
+                        i=0
+                    else:
+                        i+=1
+                res.append(temp)
+            else:
+                return res
+
+
+
+
+        
