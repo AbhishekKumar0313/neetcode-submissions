2390a31bfe6b195a60a9fbd9a9d6eb3a984d37b7
Abhishek Kumar <99200063+AbhishekKumar0313@users.noreply.github.com>
2026-07-07 22:27:26 +0530

    Add: design-hashset - submission-4

---
+++ Data Structures & Algorithms/design-hashset/submission-4.py
@@ -0,0 +1,33 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.table=[[] for _ in range(769)]
+
+    def _hash(self,key):
+        return key%769
+
+    def add(self, key: int) -> None:
+        hashvalue=self._hash(key)
+        for v in self.table[hashvalue]:
+            if v==key:
+                return None
+        self.table[hashvalue].append(key)
+
+    def remove(self, key: int) -> None:
+        hashvalue=self._hash(key)
+        for v in self.table[hashvalue]:
+            if v==key:
+                self.table[hashvalue].remove(v)
+        return None
+    def contains(self, key: int) -> bool:
+        hashvalue=self._hash(key)
+        for v in self.table[hashvalue]:
+            if v==key:
+                return True
+        return False
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
