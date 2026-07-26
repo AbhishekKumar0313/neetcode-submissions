77f1ec689c6fe666d684eddcfc1eb5864ca8d5a8
Abhishek Kumar <99200063+AbhishekKumar0313@users.noreply.github.com>
2026-07-26 21:02:23 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        counter=[0]*26
+        for i in range(len(s)):
+            counter[ord(s[i])-97]+=1
+            counter[ord(t[i])-97]-=1
+        for val in counter:
+            if val !=0:
+                return False
+
+        return True
+
+
+
+
+        
